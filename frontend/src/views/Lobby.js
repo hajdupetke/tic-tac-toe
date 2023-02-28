15bdf694a0500ab9e571280642b2364d2b4600e9
Hajdu Péter <hajdupetke@gmail.com>
2023-02-28 14:59:25 +0100

    Put server url into enviroment variable, removed unneccesary imports

--- frontend/src/views/Lobby.js
+++ frontend/src/views/Lobby.js
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import { socket } from "../logic/Socket";
 
 const Lobby = () => {
     const [roomId, setRoomId] = useState("");
